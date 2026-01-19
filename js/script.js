775af7a1c764a656b25037dc3687355ecff178ab
Salvo-Raia <salvo.raia@outlook.it>
2026-01-19 17:54:10 +0100

    Create function to deconstruct object properties

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Members Info */
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -36,3 +37,29 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+/* DOM Elements */ 
+const teamCardElement = document.getElementById("team-cards-container"); 
+const cardHtml = generateMemberCard(member);
+
+
+/* Functions */
+/**
+ * This functions allows to convert an object keys into variables and to use them in a ready layout to print into DOM.
+ * @param {object} member An object to destructurate into variables.
+ * @returns {string} A string ready to be printed into DOM, full with object properties destructurate into variables.
+ */
+function generateMemberCard (member) {
+    const {name, role, img} = member; 
+const cardHtml = `<div class="member-card d-flex align-items-center gap-3 text-white bg-dark">
+                    <div class="member-image">
+                      <img src="./img/${img}" alt="${name}">
+                    </div>
+                    <div class="member-info d-flex flex-column gap-3">
+                      <h3 class="m-0">${nome}</h3>
+                      <p class="m-0">${role}</p>
+                      <address class="m-0"><a href="#">${email}</a></address> 
+                    </div>
+                  </div>`; 
+return cardHtml; 
+}
